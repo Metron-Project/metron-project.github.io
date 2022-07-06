80f7ab98555efe51c6d4716226d74b0438170c79
Brian Pepple <bdpepple@gmail.com>
2022-07-06 03:59:02 +0000

    Fix footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,7 +116,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} The Metron Project. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
